3fbfdd5ef4f3d172be210342b35f1d960f53eaea
Vlad Vinogradov <vladt9@yandex.ru>
2015-12-02 03:25:49 +0300

    Implemented controller part

--- Life-safety/Life-safety/MainWindow.xaml.cs
+++ Life-safety/Life-safety/MainWindow.xaml.cs
@@ -87,5 +87,56 @@ namespace Life_safety
         {
             windowManager.UpdateSubstanceName(e.AddedItems[0].ToString());
         }
+
+        private void substanceStateBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Core.DamageParams.SubstanceStateType state = Core.DamageParams.SubstanceStateType.None;
+            if (substanceStateBox.SelectedIndex == 0) state = Core.DamageParams.SubstanceStateType.Gas;
+            if (substanceStateBox.SelectedIndex == 1) state = Core.DamageParams.SubstanceStateType.Fluid;
+            if (state == Core.DamageParams.SubstanceStateType.None) return;
+            windowManager.UpdateSubstanceState(state);
+        }
+
+        private void substanceMassBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            windowManager.UpdateSubstanceMass(Convert.ToSingle((string)e.AddedItems[0]));
+        }
+
+        private void windSpeedBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            windowManager.UpdateWindSpeed(Convert.ToSingle((string)e.AddedItems[0]));
+        }
+
+        private void airTypeBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Core.DamageParams.AirType airType = Core.DamageParams.AirType.None;
+            if (airTypeBox.SelectedIndex == 0) airType = Core.DamageParams.AirType.Inversion;
+            if (airTypeBox.SelectedIndex == 1) airType = Core.DamageParams.AirType.Isotermia;
+            if (airTypeBox.SelectedIndex == 2) airType = Core.DamageParams.AirType.Convection;
+            if (airType == Core.DamageParams.AirType.None) return;
+            windowManager.UpdateAirType(airType);
+            //windowManager.UpdateAirType()
+        }
+
+        private void overflowTypeBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Core.DamageParams.OverflowType overflowType = Core.DamageParams.OverflowType.None;
+            if (overflowTypeBox.SelectedIndex == 0) overflowType = Core.DamageParams.OverflowType.VPoddon;
+            if (overflowTypeBox.SelectedIndex == 1) overflowType = Core.DamageParams.OverflowType.Free;
+            if (overflowType == Core.DamageParams.OverflowType.None) return;
+            windowManager.UpdateOverflow(overflowType);
+        }
+
+        private void temperatureTypeBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Core.DamageParams.TemperatureType temperatureType = Core.DamageParams.TemperatureType.None;
+            if (temperatureTypeBox.SelectedIndex == 0) temperatureType = Core.DamageParams.TemperatureType.Freezy;
+            if (temperatureTypeBox.SelectedIndex == 1) temperatureType = Core.DamageParams.TemperatureType.Cold;
+            if (temperatureTypeBox.SelectedIndex == 2) temperatureType = Core.DamageParams.TemperatureType.Norm;
+            if (temperatureTypeBox.SelectedIndex == 3) temperatureType = Core.DamageParams.TemperatureType.Warm;
+            if (temperatureTypeBox.SelectedIndex == 4) temperatureType = Core.DamageParams.TemperatureType.Hot;
+            if (temperatureType == Core.DamageParams.TemperatureType.None) return;
+            windowManager.UpdateTemperature(temperatureType);
+        }
     }
 }
